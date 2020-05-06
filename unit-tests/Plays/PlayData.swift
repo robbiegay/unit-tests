e22d0356a7da8f455368032b4caf9e8b079ed8de
Robbie Gay <robbiecgay@gmail.com>
2020-05-06 16:04:11 -0400

    Worked through more of the Unit Tests tutorial

--- unit-tests/Plays/PlayData.swift
+++ unit-tests/Plays/PlayData.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
-    var wordCounts = [String: Int]()
+    var wordCounts: NSCountedSet!
+    private(set) var filteredWords = [String]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
@@ -19,11 +20,25 @@ class PlayData {
                 // Creates a new array including any items that match the criteria
                 // $0 is shorthand for the frist parameter
                 allWords = allWords.filter { $0 != "" }
-                for word in allWords {
-                    wordCounts[word, default: 0] += 1
-                }
-                allWords = Array(wordCounts.keys)
+                
+                wordCounts = NSCountedSet(array: allWords)
+                let sorted = wordCounts.allObjects.sorted { wordCounts.count(for: $0) > wordCounts.count(for: $1) }
+                allWords = sorted as! [String]
             }
+        }
+        
+        applyUserFilter("swift")
+    }
+    
+    func applyFilter(_ filter: (String) -> Bool) {
+        filteredWords = allWords.filter(filter)
+    }
+    
+    func applyUserFilter(_ input: String) {
+        if let userNumber = Int(input) {
+            applyFilter { self.wordCounts.count(for: $0) >= userNumber }
+        } else {
+            applyFilter { $0.range(of: input, options: .caseInsensitive) != nil }
         }
     }
 }
